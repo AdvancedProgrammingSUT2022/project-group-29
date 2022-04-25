dac5b6735690b727e58cf5a93f7ba45225370321
ErfanM0N <erfanmhd396@gmail.com>
2022-04-25 22:56:26 +0430

    some bugs fixed

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -48,12 +48,7 @@ public class MainController {
         ArrayList<User> users = new ArrayList<>();
         User user;
         for (int i = 1; i <= count; i++) {
-<<<<<<< HEAD
             Matcher matcher = Pattern.compile("-(p|-player)" + i + " (?<username>\\S+)").matcher(command);
-=======
-            Matcher matcher = Pattern.compile(".*-(p|-player)" + i + " (?<username>\\S+).*").matcher(command);
-
->>>>>>> origin/main
             if (matcher.find()) {
                 String username = matcher.group("username");
                 if ((user = isExistUsername(username)) == null)
---
+++ src/main/java/controllers/unitController.java
@@ -0,0 +1,10 @@
+package controllers;
+
+import models.MilitaryUnit;
+import models.Unit;
+
+public class unitController {
+    public void changePlace(int x, int y, MilitaryUnit unit) {
+
+    }
+}
