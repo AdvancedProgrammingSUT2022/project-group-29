95511eccc3143cb4728964ca8e264816d44abe02
ErfanM0N <erfanmhd396@gmail.com>
2022-05-01 03:33:12 +0430

    1

--- src/main/java/models/Tile.java
+++ src/main/java/models/Tile.java
@@ -10,12 +10,10 @@ public class Tile {
     private Unit civilian ;
     private MilitaryUnit militaryUnit ;
 
-    public Tile(int x, int y, TerrainAndFeature terrain, TerrainAndFeature feature, Resource resource) {
+    public Tile(int x, int y, TerrainAndFeature terrain) {
         this.x = x;
         this.y = y;
         this.terrain = terrain;
-        this.feature = feature;
-        this.resource = resource;
     }
 
     public void setCivilian(Unit civilian) {
